bdb2ef60bdeb8708549db0f1ac220f3250309aaa
Luis Arias <lugearma@gmail.com>
2016-07-27 22:20:23 -0500

    cover all screen with squares

--- BasicTapAnimation/ViewController.swift
+++ BasicTapAnimation/ViewController.swift
@@ -9,26 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
 
     @IBAction func animateButtonPressed(sender: UIButton) {
         
+        let screenHeight = self.view.frame.size.height
+        let screenWidth = self.view.frame.size.width
+        
         let size : CGFloat = CGFloat(arc4random_uniform(40)) + 20
-        let yPositon : CGFloat = CGFloat(arc4random_uniform(200)) + 20
-        let duration = 1.0
+        let xPositon : CGFloat = CGFloat(arc4random_uniform(UInt32(screenWidth))) - 20
+        let duration = 2.0
         let delay = 0.0
         let options = UIViewAnimationOptions.CurveLinear
         let square = UIView()
         
         square.backgroundColor = UIColor.redColor()
-        square.frame = CGRect(x: 0, y: yPositon, width: size, height: size)
-        
+        square.frame = CGRect(x: xPositon, y: screenHeight, width: size, height: size)
+
         self.view.addSubview(square)
         
         UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
             square.backgroundColor = UIColor.blueColor()
             
-            square.frame = CGRect(x: 320-50, y: yPositon, width: size, height: size)
+            square.frame = CGRect(x: xPositon, y: 0, width: size, height: size)
             }, completion: { animationFinished in
                 square.removeFromSuperview()
         })
